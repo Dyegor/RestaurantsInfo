83fbfff9b050853826ecab71958ce34c18a3b4ab
Dyegor <yegorov.dmt@gmail.com>
2020-04-25 17:08:27 +1200

    Fotter message from appsettins file added

--- RestaurantsInfo/Pages/Restaurants/List.cshtml.cs
+++ RestaurantsInfo/Pages/Restaurants/List.cshtml.cs
@@ -12,6 +12,7 @@ namespace RestaurantsInfo.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
+        public string Message { get; set; }
         public readonly IConfiguration config;
         public readonly IRestaurantData restaurantData;
 
@@ -26,6 +27,7 @@ namespace RestaurantsInfo.Pages.Restaurants
         }
         public void OnGet(string searchTerm)
         {
+            Message = config["Message"];
             Restaurants = restaurantData.GetRestaurantByName(SearchTerm);
         }
     }
